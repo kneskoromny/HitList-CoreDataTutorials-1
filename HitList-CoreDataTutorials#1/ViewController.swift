7f76a660ca76a5ac24fef97a458636f6f44b90be
kneskoromny <kneskoromny@gmail.com>
2021-08-08 11:23:20 +0300

    Initial Commit

---
+++ HitList-CoreDataTutorials#1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HitList-CoreDataTutorials#1
+//
+//  Created by Кирилл Нескоромный on 08.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
